592adb9d026e2f1f2241ff1216f20f39870969b9
Saadman Rafat <saadman@outlook.com>
2025-05-21 08:05:46 +0600

    Update data generator functions - Fix rescaling from 1./225 to correct 1./255 - Add conditional rescaling for pre-normalized data - Update to use TensorFlow's ImageDataGenerator API

--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
-from keras.preprocessing.image import ImageDataGenerator as IDG
-from keras.utils import to_categorical
-
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import to_categorical
 
 def data_generator(X, y, num_classes, batch_size=32):
-    return IDG(rescale=1. / 225, shear_range=0.2, zoom_range=0.2, horizontal_flip=True) \
-        .flow(X, to_categorical(y, num_classes), batch_size=batch_size)
+    """Create a data generator with data augmentation"""
+    gen = ImageDataGenerator(
+        rescale=1./255 if X.dtype != 'float32' else None,  # Skip rescaling if already normalized
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True
+    )
+    return gen.flow(X, to_categorical(y, num_classes), batch_size=batch_size)
